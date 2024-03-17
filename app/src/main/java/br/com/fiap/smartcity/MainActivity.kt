33765cc51f6f822e876d45ea4085598544791229
Laube 😎 <lucas.laube.f@gmail.com>
2024-03-17 20:04:22 -0300

    fix: navigation

--- app/src/main/java/br/com/fiap/smartcity/MainActivity.kt
+++ app/src/main/java/br/com/fiap/smartcity/MainActivity.kt
@@ -21,13 +21,6 @@ class MainActivity : ComponentActivity() {
             SmartcityTheme {
                 navController = rememberNavController()
                 SetupNavGraph(navController = navController)
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LoginScreen(navController = rememberNavController())
-                }
             }
         }
     }
